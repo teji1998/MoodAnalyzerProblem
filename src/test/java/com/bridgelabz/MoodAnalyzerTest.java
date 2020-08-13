c6bda1f024927313434020757b520eedad984222
“teji1998” <tejasvinirpk@gmail.com>
2020-08-13 06:41:07 +0530

    Added:Function to analyze the mood.

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -1,4 +1,28 @@
 package com.bridgelabz;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class MoodAnalyzerTest {
+
+	@Test
+	public void testHappyMood() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String moodState = moodAnalyzer.analyzeMood("This is a Happy Message");
+		Assert.assertEquals("HAPPY", moodState);
+	}
+
+	@Test
+	public void testSadMood() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String moodState = moodAnalyzer.analyzeMood("This is a Sad Message");
+		Assert.assertEquals("SAD", moodState);
+	}
+
+	@Test
+	public void testAnyMood() {
+		MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
+		String moodState = moodAnalyzer.analyzeMood("This is a Any Message");
+		Assert.assertEquals("HAPPY", moodState);
+	}
 }
