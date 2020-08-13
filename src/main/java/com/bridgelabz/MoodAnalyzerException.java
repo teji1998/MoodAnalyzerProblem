c68f98a945c8fce2b0d32021d0decb7819b19887
“teji1998” <tejasvinirpk@gmail.com>
2020-08-13 10:53:43 +0530

    Added:Exception for null and empty message

---
+++ src/main/java/com/bridgelabz/MoodAnalyzerException.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+public class MoodAnalyzerException extends RuntimeException{
+
+	enum ExceptionType{
+		EMPTY, NULL
+	}
+
+	ExceptionType type;
+
+	public MoodAnalyzerException(ExceptionType type, String message) {
+		super(message);
+		this.type = type;
+	}
+}
