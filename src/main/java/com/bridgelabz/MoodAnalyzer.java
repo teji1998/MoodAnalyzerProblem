bf0fc9938b5397923082f1b16b1e35ac179b4d66
“teji1998” <tejasvinirpk@gmail.com>
2020-08-13 06:37:05 +0530

    Added:Function to analyze the mood.

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,4 +1,12 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
+	public String analyzeMood(String moodMessage){
+
+		if (moodMessage.contains("Sad")) {
+			return "SAD";
+		} else {
+			return "HAPPY";
+		}
+	}
 }
