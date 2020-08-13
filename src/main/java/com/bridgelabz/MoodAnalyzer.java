ad7becb79a9065bd54f08951ffdc1257c43f907b
“teji1998” <tejasvinirpk@gmail.com>
2020-08-13 10:52:24 +0530

    Added:Exception for null and empty message

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -12,13 +12,17 @@ public class MoodAnalyzer {
 
 		public String analyzeMood() {
 			try {
+				if(message.length() == 0){
+					throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY, "EmptyMood");
+				}
 				if (message.contains("Sad")) {
 					return "SAD";
 				} else {
 					return "HAPPY";
 				}
 			}catch (NullPointerException e){
-				return "Happy";
+				throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL, "Happy");
+
 			}
 		}
 }
