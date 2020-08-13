5a7ad49cb202da777c9de567aa8bd03d66d354e5
“teji1998” <tejasvinirpk@gmail.com>
2020-08-13 07:51:10 +0530

    Refactored:Parameterised constructor is used to pass the message.

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,12 +1,20 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
-	public String analyzeMood(String moodMessage){
 
-		if (moodMessage.contains("Sad")) {
-			return "SAD";
-		} else {
-			return "HAPPY";
+	    String message;
+
+		MoodAnalyzer(){}
+
+		MoodAnalyzer(String message){
+			this.message = message;
+		}
+
+		public String analyzeMood() {
+			if(message.contains("Sad")){
+				return "SAD";
+			}else{
+				return "HAPPY";
+			}
 		}
 	}
-}
