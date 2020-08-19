ab77ee3b5d61bc86c25f0431af62bfea6d4164dc
“teji1998” <tejasvinirpk@gmail.com>
2020-08-19 16:27:07 +0530

    Added:Corrected indentations and spacing.

--- src/main/java/com/bridgelabz/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/MoodAnalyzer.java
@@ -1,29 +1,27 @@
 package com.bridgelabz;
 
 public class MoodAnalyzer {
+	String message;
 
-	    String message;
+	MoodAnalyzer(){}
 
-		MoodAnalyzer(){}
+	MoodAnalyzer(String message){
+		this.message = message;
+	}
 
-		MoodAnalyzer(String message){
-			this.message = message;
-		}
-
-		public String analyzeMood() {
-			try {
-				if(message.length() == 0){
-					throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY, "EmptyMood");
-				}
-				if (message.contains("Sad")) {
-					return "SAD";
-				} else {
-					return "HAPPY";
-				}
-			}catch (NullPointerException e){
-				throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL, "Happy");
-
+	public String analyzeMood() {
+		try {
+			if (message.length() == 0) {
+				throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.EMPTY, "EmptyMood");
 			}
+			if (message.contains("Sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
+			throw new MoodAnalyzerException(MoodAnalyzerException.ExceptionType.NULL, "Happy");
 		}
+	}
 }
 
